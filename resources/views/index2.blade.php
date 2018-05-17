4951246788a9445a3e592295ddeb1d718b9ce40e
Fernando Gomes <lfgsnando@gmail.com>
2018-05-16 23:32:15 -0300

    Limpando código

--- resources/views/index2.blade.php
+++ resources/views/index2.blade.php
@@ -20,93 +20,48 @@
   </head>
   <body>
     <div id="map"></div>
+    <script src="https://maps.google.com/maps/api/js?key=AIzaSyBhsjvabUuDHzH667B7e9YRTdPEW8U_iF4&sensor=false" type="text/javascript"></script>
 
-    <script>
-      var markers = Array();
-      // This example displays a marker at the center of Australia.
-      // When the user clicks the marker, an info window opens.
+    <script type="text/javascript">
+      var locations = Object.values(<?php echo json_encode($buracos); ?>);
 
-      var image = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png';
+      var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 11.47,
+        center: new google.maps.LatLng(-1.3631285, -48.4657668),
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      });
 
-      function getDados() {
-        var marcador = new google.maps.Marker({
-          position: {lat: -25.363, lng: 131.044},
-          map: map,
-          title: 'Uluru (Ayers Rock)',
-          icon: image
-        });
-        markers.push(marcador);
-        markers.push(marcador);
-        markers.push(marcador);
-        markers.push(marcador);
-      }
+      var infowindow = new google.maps.InfoWindow();
+      var marker, i;
+      var markers = [];
 
-      function initMap() {
-        getDados();
-        var buracos = Object.values(<?php echo json_encode($buracos); ?>);
-
-        var belem = {lat: -1.3631285, lng: -48.4657668};
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 11.47,
-          center: belem
+      for (var buraco in locations) {
+        marker = new google.maps.Marker({
+        position: new google.maps.LatLng(parseFloat(locations[buraco].lat), parseFloat(locations[buraco].lon)),
+        map: map
         });
 
-        for (var buraco in buracos) {
-          //console.log(buracos[buraco].descricao);
-          var position = {lat: parseFloat(buracos[buraco].lat), lng: parseFloat(buracos[buraco].lon)};
-          var marker = new google.maps.Marker({
-            position: position,
-            map: map,
-            title: 'Uluru (Ayers Rock)'
-          });
-          google.maps.event.addListener(marker, 'click', (function(marker) {
-           return function() {
-               var content =
-               '<div id="content">'+
-               '<div id="siteNotice">'+
-               '</div>'+
-               '<h1 id="firstHeading" class="firstHeading">'+buracos[buraco].descricao+'</h1>'+
-               '<div id="bodyContent">'+
-               '<p>Impacto: '+buracos[buraco].impacto+'</p>'+
-               '<img src="https://firebasestorage.googleapis.com/v0/b/tapa-buraco-256ba.appspot.com/o/'+buracos[buraco].imagem+'?alt=media&token=1550f920-f361-41c1-8457-8b9bc5ac26d8" alt="Buraco" width="150" height="150">'+
-               '</div>'+
-               '</div>';
-               console.log('<h1 id="firstHeading" class="firstHeading">'+buracos[buraco].descricao+'</h1>');
-               infowindow.setContent(content);
-               infowindow.open(map, marker);
-           }
-         })(marker));
+        markers.push(marker);
 
-        }
-
-        var contentString = '<div id="content">'+
+        google.maps.event.addListener(marker, 'mouseover', (function(marker, i) {
+          return function() {
+            infowindow.setContent('<div id="content">'+
             '<div id="siteNotice">'+
             '</div>'+
-            '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
+            '<h1 id="firstHeading" class="firstHeading">'+locations[buraco].descricao+'</h1>'+
             '<div id="bodyContent">'+
-            '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-            'sandstone rock formation in the southern part of the '+
-            'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-            'south west of the nearest large town, Alice Springs; 450&#160;km '+
-            '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-            'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-            'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-            'Aboriginal people of the area. It has many springs, waterholes, '+
-            'rock caves and ancient paintings. Uluru is listed as a World '+
-            'Heritage Site.</p>'+
-            '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-            'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-            '(last visited June 22, 2009).</p>'+
+            '<p>Impacto: '+locations[buraco].impacto+'</p>'+
+            '<img src="https://firebasestorage.googleapis.com/v0/b/tapa-buraco-256ba.appspot.com/o/'+locations[buraco].imagem+'?alt=media&token=1550f920-f361-41c1-8457-8b9bc5ac26d8" alt="Buraco" width="150" height="150">'+
             '</div>'+
-            '</div>';
-
-        var infowindow = new google.maps.InfoWindow({
-          content: contentString
-        });
+            '</div>');
+            infowindow.open(map, marker);
+          }
+        })(marker, i));
       }
+      console.log(markers[0]);
+
+
     </script>
-    <script async defer
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhsjvabUuDHzH667B7e9YRTdPEW8U_iF4&callback=initMap">
-    </script>
+
   </body>
 </html>
